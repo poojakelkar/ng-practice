fd3295ea5186d41d25f8371639196092107d987f
Pooja Kelkar <poojakelkar53@gmail.com>
2022-02-24 20:51:17 +0530

    featue: added title and task note

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,67 @@
+import { useState } from "react";
 import "./index.css";
 
-export default function App() {
-    const Card = ({ title, task }) => {
-        return (
-            <div className='card'>
-                <h2>{title}</h2>
-                <span>{task}</span>
-            </div>
-        );
-    };
+const Card = ({ title, task }) => {
+    return (
+        <div className='card'>
+            <h2>{title}</h2>
+            <span>{task}</span>
+        </div>
+    );
+};
 
-    const notesData = [
-        {
-            title: "whar are you doing",
-            task: "what task you added",
-        },
-        {
-            title: "add task name",
-            task: "task activities",
-        },
-        {
-            title: "abcd",
-            task: "123456",
-        },
-    ];
+const notesData = [
+    {
+        id: 1,
+        title: "whar are you doing",
+        task: "what task you added",
+    },
+    {
+        id: 2,
+        title: "add task name",
+        task: "task activities",
+    },
+    {
+        id: 3,
+        title: "abcd",
+        task: "123456",
+    },
+];
+
+export default function App() {
+    const [noteTitle, setnoteTitle] = useState("");
+    const [notetask, setTask] = useState("");
+    const [notes, setNotes] = useState(notesData);
+
+    const notesHandler = () => {
+        setNotes((prev) => [
+            ...prev,
+            { id: Math.random(), title: noteTitle, task: notetask },
+        ]);
+    };
 
     return (
         <div className='App'>
             <div>
                 <form className='notes-form'>
                     <h2>Notes App</h2>
-                    <input />
-                    <textarea />
-                    <button className='btn-notes'>Add note</button>
+                    <input
+                        type='text'
+                        value={noteTitle}
+                        onChange={(e) => setnoteTitle(e.target.value)}
+                    />
+                    <textarea
+                        type='text'
+                        value={notetask}
+                        onChange={(e) => setTask(e.target.value)}
+                    />
                 </form>
+                <button className='btn-notes' onClick={notesHandler}>
+                    Add note
+                </button>
 
                 <section className='notes'>
-                    {notesData.map((note) => {
+                    {notes.map((note) => {
                         return (
                             <Card title={note.title} task={note.task}></Card>
                         );
