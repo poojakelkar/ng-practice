983a875023ac0e1b59d8b3fa627a168fc522792c
Pooja Kelkar <poojakelkar53@gmail.com>
2022-02-24 18:24:44 +0530

    feature: Added card and title using props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,31 @@
-import { useEffect, useState } from "react";
 import "./index.css";
 
 export default function App() {
-    const [counter, setCounter] = useState(0);
-
-    function clickHandler() {
-        console.log(counter);
-        setCounter((counter) => counter + 1);
-    }
-    useEffect(() => {
-        console.log("after render", counter);
-    });
+    const Card = ({ studentName, devRoute }) => {
+        return (
+            <div className='card'>
+                <h2>{studentName}</h2>
+                <span>{devRoute}</span>
+            </div>
+        );
+    };
 
     return (
         <div className='App'>
-            <h1 className='app-header'>{counter}</h1>
-            <button onClick={clickHandler}>+</button>
+            <div>
+                <form className='notes-form'>
+                    <h2>Notes App</h2>
+                    <input />
+                    <textarea />
+                    <button className='btn-notes'>Add note</button>
+                </form>
+
+                <section className='notes'>
+                    <Card studentName='pooja' devRoute='Adv front end' />
+                    <Card studentName='neha' devRoute='Adv full stack' />
+                    <Card studentName='priti' devRoute='front end' />
+                </section>
+            </div>
         </div>
     );
 }
